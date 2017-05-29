5d4e70e15f6b6407071a93ed46a7da66381dacb4
Daniel Andrus <danieleandrus@gmail.com>
2017-05-29 00:03:31 +0000

    Rename '@StrangePan' in header to 'Twitter'. Simplifies header menu.

--- content/nav_main.php
+++ content/nav_main.php
@@ -3,6 +3,6 @@
 		<li><a href="<?=$relRoot;?>">Home</a></li>
 		<li><a href="<?=$relRoot;?>?p=resume">Resume</a></li>
 		<li><a href="<?=$relRoot;?>?p=projects">Projects</a></li>
-		<li><a href="https://twitter.com/StrangePan" class="external">@StrangePan<i class="fa fa-external-link" aria-hidden="true"></i></a></li>
+		<li><a href="https://twitter.com/StrangePan" class="external">Twitter<i class="fa fa-external-link" aria-hidden="true"></i></a></li>
 	</ul>
 </nav>
