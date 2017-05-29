36ef8f0986548f14f3475cf4b545b90404e659db
Daniel Andrus <danieleandrus@gmail.com>
2017-05-29 00:40:02 +0000

    Improve header padding. Clean up resume page, remove paper effect (looked weird on small screens). Improved button styles.

--- pages/resume.php
+++ pages/resume.php
@@ -1,13 +1,16 @@
 <?php $Parsedown = new Parsedown(); ?>
-
-<a class="download independent" href="files/resume.pdf" target="_blank" />Download PDF</a>
+<div class="button-row">
+	<a class="download independent" href="files/resume.pdf" target="_blank" />Download PDF</a>
+</div>
 
 <!-- About me information section -->
 <article class="document">
-	<?php echo $Parsedown->text(file_get_contents("content/resume.md")) ?>
+	<?=$Parsedown->text(file_get_contents("content/resume.md"))?>
 	<footer>
-		Last updated <span class="timestamp"><?php echo date("F d, Y", filemtime("content/resume.md")); ?></span>
+		Last updated <span class="timestamp"><?=date("F d, Y", filemtime("content/resume.md"))?></span>
 	</footer>
 </article>
 
-<a class="download independent" href="files/resume.pdf" target="_blank" />Download PDF</a>
+<div class="button-row">
+	<a class="download independent" href="files/resume.pdf" target="_blank" />Download PDF</a>
+</div>
